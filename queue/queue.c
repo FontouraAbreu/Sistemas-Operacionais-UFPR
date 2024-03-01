6f5b68aea42d430d0bdd170c3ea6fba5bb29cab7
Vinícius Fontoura <v.abreu.fontoura@gmail.com>
2024-03-01 20:17:01 -0300

    [created] necessary assignment files

---
+++ queue/queue.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "queue.h"
+
+int queue_size (queue_t *queue) {
+    return 0;
+}
+
+void queue_print (char *name, queue_t *queue, void print_elem (void*) ) {
+    return;
+}
+
+int queue_append (queue_t **queue, queue_t *elem) {
+    return 0;
+}
+
+int queue_remove (queue_t **queue, queue_t *elem) {
+    return 0;
+}
