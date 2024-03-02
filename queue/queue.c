f9917db9648cf0fcc66620c70a3220d258d65b7d
Vinícius Fontoura <v.abreu.fontoura@gmail.com>
2024-03-02 01:01:03 -0300

    [add] queue_remove implementation, stderr messages

--- queue/queue.c
+++ queue/queue.c
@@ -29,17 +29,20 @@ void queue_print (char *name, queue_t *queue, void print_elem (void*) ) {
 int queue_append (queue_t **queue, queue_t *elem) {
     // - a fila deve existir
     if (queue == NULL) {
+        fprintf(stderr, "queue_append: fila não existe\n");
         return -1;
     }
 
     // - o elemento deve existir
     if (elem == NULL) {
+        fprintf(stderr, "queue_append: elemento não existe\n");
         return -2;
     }
 
     // - o elemento nao deve estar em outra fila
     // se o elemento já está em outra fila, então elem->next e elem->prev não podem ser NULL
     if (elem->next != NULL || elem->prev != NULL) {
+        fprintf(stderr, "queue_append: elemento já está em outra fila\n");
         return -3;
     }
 
@@ -62,5 +65,53 @@ int queue_append (queue_t **queue, queue_t *elem) {
 }
 
 int queue_remove (queue_t **queue, queue_t *elem) {
+    // - a fila deve existir
+    if (queue == NULL) {
+        fprintf(stderr, "queue_remove: fila não existe\n");
+        return -1;
+    }
+
+    // - a fila nao deve estar vazia
+    if (queue_size(*queue) == 0) {
+        fprintf(stderr, "queue_remove: fila está vazia\n");
+        return -2;
+    }
+
+    // - o elemento deve existir
+    if (elem == NULL) {
+        fprintf(stderr, "queue_remove: elemento não existe\n");
+        return -3;
+    }
+
+    // - o elemento deve pertencer a fila indicada
+    queue_t *current_node = *queue;
+    do {
+        if (current_node == elem) {
+            break;
+        }
+        current_node = current_node->next;
+
+    } while (current_node != *queue);
+
+    if (current_node != elem) {
+        fprintf(stderr, "queue_remove: elemento não pertence à fila indicada\n");
+        return -4;
+    } else {
+        // caso nenhuma das condições acima seja verdadeira, então o elemento pode ser removido da fila
+        // se a fila tiver apenas um elemento
+        if (queue_size(*queue) == 1) {
+            *queue = NULL;
+        } else { // se a fila tiver mais de um elemento
+            elem->prev->next = elem->next;
+            elem->next->prev = elem->prev;
+            // se o elemento a ser removido é o primeiro da fila
+            if (*queue == elem) {
+                *queue = elem->next;
+            }
+        }
+        // reseta os ponteiros do elemento removido
+        elem->next = NULL;
+        elem->prev = NULL;
+    }
     return 0;
 }
