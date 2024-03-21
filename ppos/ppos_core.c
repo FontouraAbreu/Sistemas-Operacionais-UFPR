87e6c4d8f85bcea0285e1199833b839bdbe22f2d
Vinícius Fontoura <v.abreu.fontoura@gmail.com>
2024-03-21 16:12:20 -0300

    [wip] fixing task switching and exit

--- ppos/ppos_core.c
+++ ppos/ppos_core.c
@@ -6,6 +6,9 @@
 //#define DEBUG
 #define STACKSIZE 64*1024	/* tamanho de pilha das threads */
 
+task_t current_task, main_task;
+ucontext_t old_context, current_context, main_context;
+int task_count = 0;
 
 void ppos_init () {
     setvbuf(stdout, 0, _IONBF, 0);
@@ -27,7 +30,7 @@ int task_init(task_t *task, void (*start_func)(void *), void *arg) {
 
     makecontext(&task->context, (void (*)(void)) start_func, 1, arg);
 
-    task->id = rand();
+    task->id = task_count++;
     task->status = 0;
     #ifdef DEBUG
         printf("[task_init] Tarefa %d criada\n", task->id);
@@ -36,19 +39,37 @@ int task_init(task_t *task, void (*start_func)(void *), void *arg) {
 }
 
 int task_switch(task_t *task) {
-    ucontext_t current_context;
-    getcontext(&current_context);
+    // saving current context
+    if (getcontext(&old_context) == -1) {
+        perror("[task_switch] Erro ao obter o contexto atual: ");
+        return -1;
+    }
 
-    swapcontext(&current_context, &task->context);
+    current_context = task->context;
+
+    #ifdef DEBUG
+        printf("[task_switch] Trocando contexto para %d\n", task->id);
+    #endif
+
+    if (swapcontext(&old_context, &current_context) == -1) {
+        perror("[task_switch] Erro ao trocar de contexto: ");
+        return -1;
+    }
 
     return 0;
 }
 
 void task_exit(int exit_code) {
-    return;
+    #ifdef DEBUG
+        printf("[task_exit] Tarefa %d encerrada\n", task_id());
+    #endif
+    if (exit_code != 0) {
+        perror("[task_exit] Erro na finalização da tarefa: ");
+    }
+    swapcontext(&current_context, &main_context);
 }
 
 int task_id (){
-    
+    return current_task.id;
     return 0;
 }
